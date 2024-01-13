f3ec6a153e78aac3538fac59138fc89a293d8fca
Mukkara Vidya Sagar Reddy <mvsreddy2018@gmail.com>
2024-01-13 21:36:18 +0530

    Terraform - outputs

---
+++ 02-outputs/output.tf
@@ -0,0 +1,13 @@
+output "sample" {
+  value = "Hello World"
+}
+
+#Anything string in terraform need to be quoted within double quotes only.
+#Singles quotes are not supported
+
+output "sample1" {
+  value = 100
+}
+
+#Numbers and booleans (true,false) doesn't need quotes
+
