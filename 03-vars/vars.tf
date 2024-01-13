905186a456b51a266671a81babc8ff006095748f
Mukkara Vidya Sagar Reddy <mvsreddy2018@gmail.com>
2024-01-13 21:56:58 +0530

    Terraform - variables

--- 03-vars/vars.tf
+++ 03-vars/vars.tf
@@ -20,3 +20,58 @@ output "sample1" {
 output "sample-ext" {
   value = "value of sample - ${var.sample}"
 }
+
+#Variable Data Types
+#1. String
+#2. number
+#3. boolean
+
+#Variable Types
+#In Ansible - 1. Plain key value 2. list 3. Map/Dict
+
+#Terraform Variable Types
+#1. Plain
+#2. List
+#3. Map/Dict
+
+#Plain
+variable "course" {
+  default = "DevOps Training"
+}
+
+#list
+variable "courses" {
+  default = [
+    "Devops",
+    "AWS",
+    "Python"
+  ]
+}
+
+#Map
+variable "course_details" {
+  default = {
+    Devops = {
+      name = 'DevOps'
+      timing = '10am'
+      duration = 90
+    }
+    AWS = {
+      name = "AWS"
+      timing = "11am"
+      duration = 30
+    }
+  }
+}
+
+output "course" {
+  value = var.course
+}
+
+output "courses" {
+  value = var.courses[1]
+}
+
+output "course_details" {
+  value = var.course_details.Devops
+}
