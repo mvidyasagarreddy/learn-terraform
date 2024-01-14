c443f6f11d797e55b5ff44d501e87759cac5e159
Mukkara Vidya Sagar Reddy <mvsreddy2018@gmail.com>
2024-01-14 21:58:25 +0530

    Terraform - modules

---
+++ 06-modules/ec2/main.tf
@@ -0,0 +1,44 @@
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.example.id
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.sg.id]
+
+  tags = {
+    Name = "sample"
+  }
+}
+
+data "aws_ami" "example" {
+  owners      = ["973714476881"]
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "sg" {
+  name        = "sample"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
