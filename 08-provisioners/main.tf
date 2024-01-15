f48f76718c777e15d2709e9153613d0aea1a0b2f
Mukkara Vidya Sagar Reddy <mvsreddy2018@gmail.com>
2024-01-15 09:22:00 +0530

    Terraform - provisioners

--- 08-provisioners/main.tf
+++ 08-provisioners/main.tf
@@ -24,7 +24,7 @@ resource "aws_instance" "web" {
 }
 
 data "aws_ami" "example" {
-  owners           = ["973714476881"]
-  most_recent      = true
-  name_regex = "Centos-8-Devops-Practice"
+  owners      = ["973714476881"]
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
 }
