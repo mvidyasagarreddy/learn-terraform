342f5b1bbced91474bb37f18ad29eeae00d1e8e6
Mukkara Vidya Sagar Reddy <mvsreddy2018@gmail.com>
2024-01-15 15:20:07 +0530

    Terraform - lookup,count,element

---
+++ 11-loops/for-each/main.tf
@@ -0,0 +1,36 @@
+resource "aws_instance" "web" {
+  count = length(var.instances)
+  ami           = data.aws_ami.example.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instances[count.index]
+  }
+}
+
+data "aws_ami" "example" {
+  owners      = ["973714476881"]
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "instances" {
+  default = {
+    frontend = {
+      name          = "frontend"
+      instance_type = "t2.micro"
+    }
+    catalogue = {
+      name          = "catalogue"
+      instance_type = "t3.micro"
+    }
+    cart = {
+      name          = "cart"
+      instance_type = "t3.small"
+    }
+  }
+}
