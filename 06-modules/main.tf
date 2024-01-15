7130be12d1f12d4c31f5e9d873fd26dbf6263c79
Mukkara Vidya Sagar Reddy <mvsreddy2018@gmail.com>
2024-01-15 08:31:20 +0530

    Terraform - modules

--- 06-modules/main.tf
+++ 06-modules/main.tf
@@ -1,9 +1,3 @@
-module "sample1" {
+module "ec2" {
   source = "./ec2"
-  name = sample1
-}
-
-module "sample2" {
-  source = "./ec2"
-  name = sample2
 }
--- 07-module-1/ec2/main.tf
+++ 07-module-1/ec2/main.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.example.id
-  instance_type          = "t3.micro"
+  instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.sg.id]
 
   tags = {
-    Name = "sample"
+    Name = var.name
   }
 }
 
@@ -16,7 +16,7 @@ data "aws_ami" "example" {
 
 
 resource "aws_security_group" "sg" {
-  name        = "sample"
+  name        = var.name
   description = "Allow TLS inbound traffic"
 
   ingress {
@@ -36,6 +36,8 @@ resource "aws_security_group" "sg" {
   }
 
   tags = {
-    Name = "sample"
+    Name = var.name
   }
 }
+
+variable "name" {}
