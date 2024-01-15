2c5a388d67abfe7590e3c09ce3416a946da97f18
Mukkara Vidya Sagar Reddy <mvsreddy2018@gmail.com>
2024-01-15 10:48:45 +0530

    Terraform - functions & loops

--- 12-functions/main.tf
+++ 12-functions/main.tf
@@ -5,3 +5,11 @@ variable "class" {
 output "class" {
   value = upper(var.class)
 }
+
+variable "fruits" {
+  default = ["apple", "banana", "orange"]
+}
+
+output "fruits_count" {
+  value = length(var.fruits)
+}
