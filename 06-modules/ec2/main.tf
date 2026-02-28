90a694beeddc5d4d144944a72c2cf12a15acfcca
Mukkara Vidya Sagar Reddy <mvsreddy2018@gmail.com>
2026-02-28 14:42:29 +0530

    t

--- 06-modules/ec2/main.tf
+++ 06-modules/ec2/main.tf
@@ -1,66 +1,29 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners = ["973714476881"] # Canonical
-  name_regex = "Centos-8-DevOps-Practice"
-}
-
-resource "aws_instance" "example" {
-  ami           = data.aws_ami.ubuntu.id
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.example.id
+  instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.sg.id]
-  instance_type = "t2.micro"
 
   tags = {
     Name = var.name
   }
 }
 
-# resource "aws_security_group" "allow_tls" {
-#   name        = "allow_tls"
-#   description = "Allow TLS inbound traffic and all outbound traffic"
-#   vpc_id      = aws_vpc.main.id
+data "aws_ami" "example" {
+  owners      = ["973714476881"]
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+}
 
-#   tags = {
-#     Name = "allow_tls"
-#   }
-# }
-
-# resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-#   security_group_id = aws_security_group.allow_tls.id
-#   cidr_ipv4         = aws_vpc.main.cidr_block
-#   from_port         = 443
-#   ip_protocol       = "tcp"
-#   to_port           = 443
-# }
-
-# resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
-#   security_group_id = aws_security_group.allow_tls.id
-#   cidr_ipv6         = aws_vpc.main.ipv6_cidr_block
-#   from_port         = 443
-#   ip_protocol       = "tcp"
-#   to_port           = 443
-# }
-
-# resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-#   security_group_id = aws_security_group.allow_tls.id
-#   cidr_ipv4         = "0.0.0.0/0"
-#   ip_protocol       = "-1" # semantically equivalent to all ports
-# }
-
-# resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
-#   security_group_id = aws_security_group.allow_tls.id
-#   cidr_ipv6         = "::/0"
-#   ip_protocol       = "-1" # semantically equivalent to all ports
-# }
 
 resource "aws_security_group" "sg" {
-  name = var.name
-  description = "Allow all traffic"
+  name        = var.name
+  description = "Allow TLS inbound traffic"
 
-  ingress = {
+  ingress {
     description = "SSH"
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -77,6 +40,8 @@ resource "aws_security_group" "sg" {
   }
 }
 
-variable "name" {
-  
+variable "name" {}
+
+output "public_ip" {
+  value = aws_instance.web.public_ip
 }
