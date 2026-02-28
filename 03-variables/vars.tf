bf34078b0695f91b95fa0821804f8117633c22db
Mukkara Vidya Sagar Reddy <mvsreddy2018@gmail.com>
2026-02-28 11:59:27 +0530

    t

---
+++ 03-variables/vars.tf
@@ -0,0 +1,26 @@
+variable "sample" {
+  default = 100
+}
+
+variable "sample1" {
+  default = "HelloWorld"
+}
+
+variable "sample-ext" {
+  default = "Mawa"
+}
+
+output "sample" {
+  value = var.sample
+}
+
+output "sample1" {
+  value = var.sample1
+}
+
+#Sometimes if variable/any reference with the combination of some other strings then
+#we have to access those in ${}
+
+output "sample-ext" {
+  value = "Value of sample is - ${var.sample}"
+}
