135a1630833030ddcabf1b79dc7b72f53232f0fa
Mukkara Vidya Sagar Reddy <mvsreddy2018@gmail.com>
2026-02-28 12:55:48 +0530

    t

--- 04-datasources/data.tf
+++ 04-datasources/data.tf
@@ -11,3 +11,11 @@ data "aws_ec2_spot_price" "example" {
 output "aws_ec2_spot_price" {
   value = data.aws_ec2_spot_price.example.spot_price
 }
+
+data "aws_security_group" "selected"{
+    name = "allow-all"
+}
+
+output "name" {
+  value = data.aws_security_group.selected.id
+}
