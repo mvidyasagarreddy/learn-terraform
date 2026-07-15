b48e729591f5b404f0be9d126aff9ee4f6456bb5
Mukkara Vidya Sagar Reddy <mvsreddy2018@gmail.com>
2026-07-15 07:58:38 +0530

    Chame ami name regex

--- 07-modules-1/ec2/main.tf
+++ 07-modules-1/ec2/main.tf
@@ -11,7 +11,7 @@ resource "aws_instance" "web" {
 data "aws_ami" "example" {
   owners      = ["973714476881"]
   most_recent = true
-  name_regex  = "Redhat-9-DevOps-Practice"
+  name_regex  = "RedHat-9-DevOps-Banadaru"
 }
 
 
