624a6b74d74be91a631bdafa866a37c2031dfc90
Mukkara Vidya Sagar Reddy <mvsreddy2018@gmail.com>
2026-07-16 18:22:30 +0530

    add remote state change bucket name

---
+++ 10-loops/count/main.tf
@@ -0,0 +1,30 @@
+//resource "aws_instance" "web" {
+//  count         = length(var.instances)
+//  ami           = data.aws_ami.example.id
+//  instance_type = "t3.micro"
+//
+//  tags = {
+//    Name = "HelloWorld-${count.index}"
+//  }
+//}
+
+resource "aws_instance" "web" {
+  count         = length(var.instances)
+  ami           = data.aws_ami.example.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = element(var.instances, count.index)
+  }
+}
+
+
+data "aws_ami" "example" {
+  owners      = ["973714476881"]
+  most_recent = true
+  name_regex  = "Redhat-9-DevOps-Practice"
+}
+
+variable "instances" {
+  default = ["frontend", "catalogue", "cart"]
+}
