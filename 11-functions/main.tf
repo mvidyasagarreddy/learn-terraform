c216778ec0d89df2845f1a5a8be809f679dbaa06
Mukkara Vidya Sagar Reddy <mvsreddy2018@gmail.com>
2026-07-16 18:31:17 +0530

    terraform functions

---
+++ 11-functions/main.tf
@@ -0,0 +1,52 @@
+variable "class" {
+  default = "devops"
+}
+
+## Print the same in capital leetters
+output "class" {
+  value = upper(var.class)
+}
+
+variable "fruits" {
+  default = ["apple", "banana", "orange"]
+}
+
+output "fruit_count" {
+  value = length(var.fruits)
+}
+
+variable "classes" {
+  default = {
+    devops = {
+      name   = "devops"
+      topics = ["jenkins", "docker"]
+    }
+    aws = {
+      name = "aws"
+    }
+  }
+}
+
+output "devops_topics" {
+  value = var.classes["devops"]["topics"]
+}
+
+//output "aws_topics" {
+//  value = lookup(lookup(var.classes, "aws", null), "topics", "No Topics So far")
+//}
+
+//output "aws_topics" {
+//  value = lookup(lookup(var.classes, "aws", null), "topics", null) == null ? "No AWS Training So far" : lookup(lookup(var.classes, "aws", null), "topics", null)
+//}
+
+output "fruit_4" {
+  value = element(var.fruits, 4)
+}
+
+variable "a" {
+  default = 100
+}
+
+output "a" {
+  value = var.a > 50  ? "Surplus Quantity" : "Less Quantity"
+}
